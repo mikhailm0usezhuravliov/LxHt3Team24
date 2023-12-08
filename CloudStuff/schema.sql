ab67b7ccf9835c434e5f4bb88f07562d6e42b965
MrSakuraHub <153280985+MrSakuraHub@users.noreply.github.com>
2023-12-09 00:24:07 +0200

    Update schema.sql
    Added several statement for the server' Schema

--- CloudStuff/schema.sql
+++ CloudStuff/schema.sql
@@ -1,1 +1,5 @@
+create table Users(Id int NOT NULL, Age int, Location varchar(39), UserType varchar(39), PhoneNumber varchar(15), GovId varchar(15), Email varchar(39), AppealsCount int, PRIMARY KEY(Id));
+
+create table AppealHouse(AppealID int, UserID int, PRIMARY KEY(AppealID, UserID));
+
 
